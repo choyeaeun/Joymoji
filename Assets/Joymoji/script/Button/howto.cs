1713669dcec37656ae31161ce3b827eef151101f
choyeaeun <silbia629@gmail.com>
2019-09-26 21:30:09 +0900

    add HOWTO Scene

---
+++ Assets/Joymoji/script/Button/howto.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class howto : MonoBehaviour
+{
+    private Button _button;
+    public GameObject buttonObj;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        _button = GetComponent<Button>();
+
+        _button.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("howto");
+        });
+
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        buttonObj.GetComponent<Animation>().Play();
+    }
+
+}
